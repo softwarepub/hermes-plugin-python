9aee6df3bb239b436f3b77849b115a49836db18d
notactuallyfinn <mfritzsche3108@gmail.com>
2024-07-12 12:01:28 +0200

    Fixed small bug

--- src/hermes_toml/harvest.py
+++ src/hermes_toml/harvest.py
@@ -186,7 +186,7 @@ class TomlHarvestPlugin(HermesHarvestPlugin):
                     #try to parse the string
                     try:
                         [(name, email)] = getaddresses([person])
-                        return cls.remove_forbidden_keys({"name":name, "email":email})
+                        return_list.append(cls.remove_forbidden_keys({"name":name, "email":email}))
                     except ValueError:
                         raise ValueError("Wrong string format for name (and email).")
 
